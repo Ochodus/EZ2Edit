d5041d280b75539256157110d722a67a19f9db39
Ochodus <mario1470@naver.com>
2019-12-13 23:44:40 +0900

    Test Commit

---
+++ src/ez2edit/EZ2edit.java
@@ -0,0 +1,15 @@
+package ez2edit;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class EZ2edit extends JPanel {
+	public static void main(String[] args) {
+		EZ2edit editor = new EZ2edit();
+		JFrame frame = new JFrame("EZ2Edit");
+		frame.getContentPane().add(editor);
+		frame.setBounds(200,300,430,450);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
